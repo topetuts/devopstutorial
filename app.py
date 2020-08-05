43b8f56c02c57b92be46f1cb23bcf0e271c8e726
Christopher.Llamas <Christopher.Llamas@aventine-ts.com>
2020-08-05 10:26:55 +0800

    new route

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return "hello chris"
 
+@app.route('/2')
+def hello_world():
+    return "hello 2"
+
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
